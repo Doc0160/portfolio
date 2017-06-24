e73d3527f54896065759e052dbfea95b79ece2fd
Tristan Magniez <tristan.magniez@viacesi.fr>
2017-06-24 23:55:03 +0200

    24/06/2017_23:55:00,02

--- build.bat
+++ build.bat
@@ -1,6 +1,9 @@
 @echo off
-del img\*.png
+rem del img\*.png
 php gen.php
+
+IF 1==1 (
 git add .
 git commit -m "%date%"_%time%"
 git push
+)
--- gen.php
+++ gen.php
@@ -1,4 +1,38 @@
 <?php
+function slugify($string, $replace = array(), $delimiter = '-') {
+    // https://github.com/phalcon/incubator/blob/master/Library/Phalcon/Utils/Slug.php
+    if (!extension_loaded('iconv')) {
+        throw new Exception('iconv module not loaded');
+    }
+    // Save the old locale and set the new locale to UTF-8
+    $oldLocale = setlocale(LC_ALL, '0');
+    setlocale(LC_ALL, 'en_US.UTF-8');
+    $clean = iconv('UTF-8', 'ASCII//TRANSLIT', $string);
+    if (!empty($replace)) {
+        $clean = str_replace((array) $replace, ' ', $clean);
+    }
+    $clean = preg_replace("/[^a-zA-Z0-9\/_|+ -]/", '', $clean);
+    $clean = strtolower($clean);
+    $clean = preg_replace("/[\/_|+ -]+/", $delimiter, $clean);
+    $clean = trim($clean, $delimiter);
+    // Revert back to the old locale
+    setlocale(LC_ALL, $oldLocale);
+    return $clean;
+}
+
+function text($text) {
+    if(is_array($text)) {
+        $tmp = "";
+        foreach($text as $desc) {
+            $tmp .= '<p>'.nl2br(trim($desc)).'</p>';
+        }
+        $text = $tmp;
+        unset($tmp);
+    }
+    $text = '<p>'.nl2br(trim($text)).'</p>';
+    return $text;
+}
+
 ob_start();
 
 $projects = [
@@ -7,50 +41,87 @@ $projects = [
         'link' => 'https://github.com/Doc0160/FrAnanas',
         'img' => 'https://raw.githubusercontent.com/Doc0160/FrAnanas/master/frananas.png',
         'desc' => 'FrAnanas est un framework php7.',
-        'lang' => ['PHP'],
+        'lang' => ['PHP', 'Framework'],
     ],
     [
         'name' => 'Kikipedo',
-        'desc' => 'Wiki portable.',
+        'desc' => [
+            'Serveur portable de Wiki simplifié créer dans le but de faciliter la création d\'une base de connaissance lors d\'un travail en équipe.',
+            'Ce projet a été, au départ, crée pour aider au suivi des fiches de personnage du jeux de rôle "Roll For Shoes".',
+        ],
+        'more' => [
+            'Ce programme est composé de plusieurs choses:',
+            '<ul>'
+            .'<li>un serveur http</li>'
+           .'<li>un sytéme de base de données</li>'
+           .'<li>le systéme de wiki</li>'
+           .'<li>une api rest</li>'
+           .'</ul>',
+            'La base de données est stockée dans un fichier *.db qui peut être ensuit partagé et utiliser par n\'importe qui d\'autre possédant ce logiciel.',
+            'Pour des soucis de portabilité, les fichiers de style, de script et de templates sont stockées dans l\'éxécutable même.',
+        ],
         'link' => 'https://gitla.in/Doc0160/kikipedo',
-        'lang' => ['Go'],
+        'lang' => ['Go', 'HTTP', 'Markdown', 'BoltDB'],
     ],
     [
         'name' => 'Pink Samurai',
         'link' => 'https://github.com/Doc0160/Pink-Samurai-Chat',
         'img' => 'https://raw.githubusercontent.com/Doc0160/Pink-Samurai-Chat/master/404/evil_eyes.png',
-        'desc' => 'Pink Samurai est un logicel de chat pour réseau local ... avec un design plutôt douteux.',
+        'desc' => [
+            'Pink Samurai est un logicel de chat pour réseau local ... avec un design plutôt douteux.',
+        ],
+        'more' => [
+            'Prend en charge:',
+            '<ul>'
+            .'<li>La création d\'un compte</li>'
+            .'<li>HTTPS</li>'
+            .'<li>Multiples channels</li>'
+           .'</ul>',
+        ],
         'lang' => ['Go', 'Websockets'],
     ],
     [
         'name' => 'platform.go',
         'link' => 'https://gitla.in/Doc0160/platform.go',
         'desc' => 'Wrapper de la librairie shiny pour fournir une API simple permettant d\'afficher une image. Fait aussi librairie son.',
-        'lang' => ['Go', 'Shiny'],
+        'lang' => ['Go', 'Shiny', 'GUI'],
     ],
     [
         'name' => 'NESfu**er',
         'link' => 'https://git.teknik.io/Doc0160/NESfucker',
-        'desc' => 'Nes emulateur.',
+        'desc' => [
+            'Emulateur NES NTSC.',
+            'Le processus de compilation m\'étant familier, je me suis tempté à l\'émulation. La NES étant très bien docummenté, c\'est donc le systéme que j\'ai choisi de documenter.',
+        ],
+        'more' => [
+            'Le projet prend en charge un nombre limité de mapper.',
+            'Ce projet n\'implémente pas non plus les instructions illégales du microprocesseur MOS Technology 6502.',
+        ],
         'lang' => ['Go', 'platform.go', 'NES'],
     ],
     [
         'name' => 'GBfu**er',
         'link' => 'https://git.teknik.io/Doc0160/GBfucker',
-        'desc' => 'GameBoy emulateur.',
+        'desc' => [
+            'GameBoy emulateur.',
+            'Second émulateur que j\'ai créé. Ne prend pas en charge les jeux GameBoy Color.',
+        ],
         'lang' => ['Go', 'platform.go', 'GameBoy'],
     ],
     [
         'name' => 'asm.go',
         'link' => 'https://gitla.in/Doc0160/asm.go',
-        'desc' => 'RDTSC pour Go.',
+        'desc' => [
+            'Met à disposition l\'instruction RDTSC pour Go.',
+            'Permet de faire une meusure précise de la performance d\'un programme (en cycles processeurs).',
+        ],
         'lang' => ['Go', 'asm'],
     ],
     [
         'name' => 'YLP',
         'link' => 'https://github.com/Doc0160/YLP',
-        'desc' => 'Un programme ayant pour but de secrétement changer le fond d\'écran.',
-        'lang' => ['Go', 'Windows'],
+        'desc' => 'Un programme ayant pour but de secrétement changer le fond d\'écran d\'un ordinateur windows à interval régulier.',
+        'lang' => ['Go', 'Windows', 'Blague'],
     ],
     [
         'name' => 'Melter',
@@ -67,7 +138,10 @@ $projects = [
     [
         'name' => 'VLCiBas',
         'link' => 'https://github.com/Doc0160/VLCiBaS',
-        'desc' => 'VLC is Bad at streaming. Programme qui télécharge assez de video avant de lancer VLC pour visioner pendant que la video fini de télécharger.',
+        'desc' => [
+            '"VLC is Bad at streaming" est un programme qui cherche à compenser le buffer trop petit de VLC lors de la lecture de video en streaming.',
+            'Ce programme télécharge assez de video avant de lancer VLC pour visioner sans jamais attendre la phase de buffering.',
+        ],
         'lang' => ['Go'],
     ],
 ];
@@ -82,29 +156,30 @@ function doImg(array $project) {
     $img = 'http://fakeimg.pl/320/ffffff,0/000000/?text='.urlencode($project['name']);
     if(isset($project['img'])) $img = $project['img'];
     
-    $filename = 'img/'.$project['name'].'.png';
-    $filename = str_replace('*', '-', $filename);
-    file_put_contents($filename, file_get_contents($img));
-    
-    $source = imagecreatefrompng($filename);
-    list($width, $height) = getimagesize($filename);
-    $thumb = imagecreatetruecolor(320, 320);
-    $color = imagecolorat($source, 0, 0);
-    imagefill($thumb, 0, 0, $color);
-    imagealphablending($thumb, false);
-    imagesavealpha($thumb, true);
-    
-    $nh = 320;
-    $nw = 320;
-    $ratio = $width/$height;
+    $filename = 'img/'.slugify($project['name']).'.png';
+    if(!file_exists($filename)) {
+        file_put_contents($filename, file_get_contents($img));
+        
+        $source = imagecreatefrompng($filename);
+        list($width, $height) = getimagesize($filename);
+        $thumb = imagecreatetruecolor(320, 320);
+        $color = imagecolorat($source, 0, 0);
+        imagefill($thumb, 0, 0, $color);
+        imagealphablending($thumb, false);
+        imagesavealpha($thumb, true);
+        
+        $nh = 320;
+        $nw = 320;
+        $ratio = $width/$height;
 
-    if ($nw/$nh > $ratio) {
-        $nw = $nh*$ratio;
-    } else {
-        $nh = $nw/$ratio;
+        if ($nw/$nh > $ratio) {
+            $nw = $nh*$ratio;
+        } else {
+            $nh = $nw/$ratio;
+        }
+        imagecopyresampled($thumb, $source, 320/2-$nw/2, 320/2-$nh/2, 0, 0, $nw, $nh, $width, $height);
+        imagepng($thumb, $filename);
     }
-    imagecopyresampled($thumb, $source, 320/2-$nw/2, 320/2-$nh/2, 0, 0, $nw, $nh, $width, $height);
-    imagepng($thumb, $filename);
 
     if(isset($project['link'])) echo '<a target="_blank" href="'.$project['link'].'">';
     echo '<img class="img-rounded" src="'.$filename.'">';
@@ -156,7 +231,7 @@ function doImg(array $project) {
                 var_dump($project);
                 echo '-->';
                 
-                echo '<div class="col-md-4">';
+                echo '<div class="col-lg-4">';
                 echo '<div class="thumbnail">';
                 
                 doImg($project);
@@ -169,8 +244,20 @@ function doImg(array $project) {
                     echo '</kbd>';
                 }
 
-                if(isset($project['desc'])) echo '<p>'.nl2br(trim($project['desc'])).'</p>';
-
+                echo text($project['desc']);
+                
+                if(isset($project['more'])) {
+                    echo '<button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#'.slugify($project['name']).'" aria-expanded="false" aria-controls="collapseExample">';
+                    echo 'En savoir plus';
+                    echo '</button>';
+                    
+                    echo '<div class="collapse" id="'.slugify($project['name']).'">';
+                    echo '<div class="well">';
+                    echo text($project['more']);
+                    echo '</div>';
+                    echo '</div>';
+                }
+                
                 echo '</div>';
                 echo '</div>';
                 echo '</div>';
